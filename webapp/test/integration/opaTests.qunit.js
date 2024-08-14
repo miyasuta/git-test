f07243b7c24f7768c3f46916ca7585f8d11a02d2
Mio Yasutake <yasutake_m@itg.hitachi.co.jp>
2024-08-14 04:24:23 +0000

    initial commit

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["gittest/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"git-test/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
